98dd6526419e7f578c14e736e6fdebf8fb9b1147
A-JiuA <1169942595@qq.com>
2023-06-21 20:33:53 +0800

    Update Release.yml

--- scripts/get_artifact_name.py
+++ scripts/get_artifact_name.py
@@ -3,5 +3,5 @@ import json
 
 json = json.loads(os.getenv("ARTIFACTS_LIST"))
 for i in range(len(json)):
-    os.system("echo $foldername"+str(i)+"="+json[i]["name"]+" >> $GITHUB_OUTPUT")
+    os.system("echo foldername"+str(i)+"="+json[i]["name"]+" >> $GITHUB_OUTPUT")
     
