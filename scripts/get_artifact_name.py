6ff5e7d77ca4045b02f682d92df1667d170c6525
A-JiuA <1169942595@qq.com>
2023-06-21 19:16:46 +0800

    Update Release.yml

---
+++ scripts/get_artifact_name.py
@@ -0,0 +1,7 @@
+import os
+import json
+
+json = json.loads(os.getenv("ARTIFACTS_LIST"))
+for i in range(len(json)):
+    os.system("echo $foldername"+str(i)+"="+json[i]["name"]+" >> $GITHUB_OUTPUT")
+    
