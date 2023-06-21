b208de425cef308ca903fcf55eae552a276d760e
A-JiuA <1169942595@qq.com>
2023-06-21 23:58:38 +0800

    Update Release.yml

--- scripts/get_artifact_name.py
+++ scripts/get_artifact_name.py
@@ -1,7 +1,11 @@
 import os
 import json
 
-json = json.loads(os.getenv("ARTIFACTS_LIST"))
-for i in range(len(json)):
-    os.system("echo foldername"+str(i)+"="+json[i]["name"]+" >> $GITHUB_OUTPUT")
+js = json.loads(os.getenv("ARTIFACTS_LIST"))
+c=0
+for i in js:
+    if i["name"][:3] == "WSA":
+        print("echo foldername"+str(c)+"="+i["name"]+" >> $GITHUB_OUTPUT")
+        os.system("echo foldername"+str(c)+"="+i["name"]+" >> $GITHUB_OUTPUT")
+        c+=1
     
